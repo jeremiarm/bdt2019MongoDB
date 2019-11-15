b827a4365ada618c0cf2facbbf0903ece666d516
Jeremia Ronaldo Manurung <jeremiarm5@gmail.com>
2019-11-15 08:14:52 +0700

    inisiasi awal

---
+++ provision/allhosts.sh
@@ -0,0 +1,20 @@
+# Add hostname
+sudo cp /vagrant/sources/hosts /etc/hosts
+
+# Copy APT sources list
+sudo cp /vagrant/sources/sources.list /etc/apt/
+sudo cp /vagrant/sources/mongodb-org-4.2.list /etc/apt/sources.list.d/
+
+# Add MongoDB repo key
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+
+# Update Repository
+sudo apt-get update
+# sudo apt-get upgrade -y
+
+# Install MongoDB
+sudo apt-get install -y mongodb-org
+
+# Start MongoDB
+sudo service mongod start
