0bbe200c121a8c5f8a2d37b44e63f293836f492b
Jeremia Ronaldo Manurung <jeremiarm5@gmail.com>
2019-11-18 14:25:15 +0700

    Update database.php

--- project/laravelmongodb/config/database.php
+++ project/laravelmongodb/config/database.php
@@ -98,9 +98,10 @@ return [
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
-            'options' => array(
-                'database' => 'admin' // sets the authentication database required by mongo 3
-            )
+            'options'  => [
+                'database' => 'admin',
+                //'replicaSet' => 'configReplSet'
+            ]
         ],
 
     ],
